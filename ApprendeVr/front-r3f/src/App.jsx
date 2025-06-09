9b56f4fd4a0896fbb51f158550b3499ba685ef3e
Will Vargas <wilbervargas1@gmail.com>
2025-06-09 13:40:58 -0500

    r3f basic files

---
+++ ApprendeVr/front-r3f/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import VRScene from './VRScene'
+import './App.css'
+
+const App = () => {
+  return (
+    <div style={{ height: '100vh' }}>
+      <VRScene />
+    </div>
+  )
+}
+
+export default App
