d2d340940bd32ffcd25e2e70cb379b9cb94ad0e4
Will Vargas <wilbervargas1@gmail.com>
2025-06-14 07:51:57 -0500

    default vr view in /mobile.html

--- ApprendeVr/front-r3f/src/MobileApp.jsx
+++ ApprendeVr/front-r3f/src/MobileApp.jsx
@@ -1,7 +1,25 @@
 import { Canvas } from '@react-three/fiber'
 import { XR, VRButton, Controllers, Hands } from '@react-three/xr'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { Environment, OrbitControls, Sky } from '@react-three/drei'
 import { Suspense, useState, useEffect } from 'react'
+
+function Box({ position = [0, 0.5, -5] }) {
+  return (
+    <mesh position={position}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  )
+}
+
+function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+      <planeGeometry args={[30, 30]} />
+      <meshStandardMaterial color="#53a852" />
+    </mesh>
+  )
+}
 
 function MobileScene() {
   const [hasPermission, setHasPermission] = useState(false)
@@ -25,17 +43,31 @@ function MobileScene() {
 
   return (
     <>
-      <Environment preset="sunset" />
+      {/* Luces */}
       <ambientLight intensity={0.5} />
-      <mesh position={[0, 0, -5]}>
-        <boxGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+
+      {/* Cielo */}
+      <Sky 
+        distance={450000}
+        sunPosition={[0, 1, 0]}
+        inclination={0}
+        azimuth={0.25}
+      />
+
+      {/* Elementos de la escena */}
+      <Box />
+      <Floor />
+
+      {/* Controles */}
       <OrbitControls 
         enableZoom={true}
         enablePan={false}
         enableDamping={true}
         makeDefault
+        maxPolarAngle={Math.PI / 2}
+        minDistance={2}
+        maxDistance={20}
       />
     </>
   )
@@ -45,7 +77,7 @@ function MobileApp() {
   return (
     <>
       <VRButton className="vr-button" />
-      <Canvas>
+      <Canvas shadows>
         <XR>
           <Controllers />
           <Hands />
