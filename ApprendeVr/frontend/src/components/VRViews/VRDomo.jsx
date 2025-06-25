e4e818a98710913317db4dd3182a003728bcf924
Will Vargas <wilbervargas1@gmail.com>
2025-06-25 08:23:09 -0500

    feat: Add text panel class to cone elements for improved styling

--- ApprendeVr/frontend/src/components/VRViews/VRDomo.jsx
+++ ApprendeVr/frontend/src/components/VRViews/VRDomo.jsx
@@ -481,6 +481,7 @@ const generateDomeElements = (geometryData) => {
           class="clickable"
         > 
           <a-entity
+            
             class="texto-panel"
             text={`value: ${palabra.en}; color: #FFFFFF; align: center; width: 5; wrapCount: 10; font: /fonts/Ultra-msdf/Ultra-msdf.json; fontImage: /fonts/Ultra-msdf/Ultra-msdf.png; shader: msdf`}
             position="0 0 0.11"
