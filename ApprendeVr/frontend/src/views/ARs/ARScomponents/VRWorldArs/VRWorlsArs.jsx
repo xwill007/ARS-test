cdb21df58c00b3486f913c3bce2fa5a4d69fadf4
Wilber Vargas <xxxwill007@gmail.com>
2025-07-17 11:55:06 -0500

    feat(VRWorldArs): make floor size and scale configurable for enhanced customization

--- ApprendeVr/frontend/src/views/ARs/ARScomponents/VRWorldArs/VRWorlsArs.jsx
+++ ApprendeVr/frontend/src/views/ARs/ARScomponents/VRWorldArs/VRWorlsArs.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
 import VRPlaneArs from './VRPlaneArs';
 
-const FLOOR_SIZE = 10;
-
-const VRWorldArs = ({ children, floorPosition = [0, 0, 0] }) => {
+const VRWorldArs = ({ children, floorPosition = [0, 0, 0], floorSize = 10, floorScale = [1, 1, 1] }) => {
   const { camera } = useThree();
 
   useFrame(() => {
     const { x, z } = camera.position;
-    if (Math.abs(x) > FLOOR_SIZE / 2 || Math.abs(z) > FLOOR_SIZE / 2) {
+    if (Math.abs(x) > floorSize / 2 || Math.abs(z) > floorSize / 2) {
       camera.position.y -= 0.1;
       if (camera.position.y < -10) {
         camera.position.set(0, 2, 0);
@@ -21,7 +19,7 @@ const VRWorldArs = ({ children, floorPosition = [0, 0, 0] }) => {
 
   return (
     <>
-      <VRPlaneArs size={FLOOR_SIZE} position={floorPosition} />
+      <VRPlaneArs size={floorSize} position={floorPosition} scale={floorScale} />
       {children}
     </>
   );
